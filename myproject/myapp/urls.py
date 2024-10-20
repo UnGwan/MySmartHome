353e1f7d4aed73d735ca82c9950863d4f8bf2808
UnGwan <jkj8842@gmail.com>
2024-10-20 06:10:50 +0000

    온/습도 모드 로직 추가

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('led/', views.led_control, name='led_control'),
     path('led/status', views.get_led_status, name='get_led_status'),  # LED 제어를 위한 엔드포인트
-    path('circulator/',views.circulator_control, name='circulator_control')
+    path('circulator/',views.circulator_control, name='circulator_control'),
+    path('circulator/temperature_humidity', views.get_temperature_humidity, name = 'get_temperature_humidity')
 ]
